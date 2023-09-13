62a3b699c563325303affa54b0cc0c1da7461eee
FiLLi <34349763+Filli-z@users.noreply.github.com>
2023-09-13 15:54:51 +0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
     },
-    {
-      path: '/alumni',
-      name: 'alumni',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../components/Alumini.vue')
-    },
-    {
-      path: '/footer',
-      name: 'footer',
-      component: () => import('../components/footerss.vue')
-    },
-    {
-      path:'/timeline',
-      name: 'timeline',
-      component: () => import('../components/TimeLine.vue')
-    },
-    {
-      path:'/gallery',
-      name: 'gallery',
-      component: () => import('../components/Gallery.vue')
-    },
-  ]
-})
 
-export default router
+    {
+      path: "/footer",
+      name: "footer",
+      component: () => import("../components/footerss.vue"),
+    },
+    {
+      path: "/timeline",
+      name: "timeline",
+      component: () => import("../components/TimeLine.vue"),
+    },
+    {
+      path: "/gallery",
+      name: "gallery",
+      component: () => import("../components/Gallery.vue"),
+    },
+  ],
+});
+
+export default router;
