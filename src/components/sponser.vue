4c4bfdd9b24a18d77542e9bdeb0e5831eb1082f5
FiLLi <34349763+Filli-z@users.noreply.github.com>
2024-04-22 16:18:40 +0300

    final

--- src/components/sponser.vue
+++ src/components/sponser.vue
@@ -1,40 +1,38 @@
 <template>
-      <h1 class="h1" style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif">Partners</h1>
-<div class="sponsor-container">
-    <img src="src/assets/images/Ethiopianairlineslogo.png" alt="Snow"  >
-    <img src="../assets/images/boing.png" alt="Forest" >
-    <img src="../assets/images/thinkyounglogo.jpg" alt="Mountains" >
-</div>
-
-  
+  <h1
+    class="h1"
+    style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif"
+  >
+    Partners
+  </h1>
+  <div class="sponsor-container">
+    <img src="../assets/images/ethiopianairlineslogo.png" alt="Snow" />
+    <img src="../assets/images/boing.png" alt="Forest" />
+    <img src="../assets/images/thinkyounglogo.jpg" alt="Mountains" />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-.sponsor-container  {
-     display: flex;
-    justify-content: center;
-    align-items: center;
+.sponsor-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    background-color: whitesmoke;
-
-  }
-img{
-    width:150px;
-    margin-right: 50px;   /* Horizontal Space */
+  background-color: whitesmoke;
 }
-h1{
+img {
+  width: 150px;
+  margin-right: 50px; /* Horizontal Space */
+}
+h1 {
   text-align: center;
   margin-top: 0%;
   font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
   font-weight: bolder;
   color: #093145;
-
 }
-
 </style>
